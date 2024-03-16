4d48b9eb8dd3be6adfcd65aa3cdb5298eebd6f4c
frogger523 <calvin.e.locklin@gmail.com>
2024-03-15 23:53:07 -0400

    hello

--- commands/mineflayer/navigation/come.js
+++ commands/mineflayer/navigation/come.js
@@ -9,7 +9,8 @@ module.exports = {
   },
   author: 'Pix3lPirat3',
   execute: async function(sender, command, args) {
-    if(sender.type !== 'player') return sender.reply(`[Come] This command can only be ran by a player. Use 'goto' instead.`);
+    const specificPlayer = 'BirbInMatrix'; // replace with the specific player's username
+    if(sender.username !== specificPlayer) return sender.reply(`[Come] This command can only be ran by ${specificPlayer}. Use 'goto' instead.`);
     let player = sender.player;
     let entity = bot.players[player]?.entity;
     if(!entity) return sender.reply(`[Come] I cannot see you ${player}`);
---
+++ listeners/mineflayer/FroggerSaysSmth.js
@@ -0,0 +1,30 @@
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+module.exports = {
+  object: bot,
+  event: 'messagestr',
+  description: 'Attack mobs around you',
+  reload: {
+    pre: function() {
+      
+    },
+    post: function() {
+
+    }
+  },
+  author: 'Pix3lPirat3',
+  execute: function(message, messagePosition, jsonMsg, sender, verified) {
+    // User says `[formatter]` (or generated ID?)
+    // console cmd: formatter <username> <message>
+    // and it parses the format from those 2 objects..?
+
+    // NO - running `formatter` will send a message
+    // and this listener will send the message and do the rest automatically
+
+    if (sender === 'frogger523') {
+      // Do something when frogger523 says something
+      // For example, make the bot reply
+      bot.chat('Hello, frogger523! You said: ' + message);
+    }
+  }
+}
